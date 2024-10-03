dda7b39aaf7e9e8b76213c919f19af08deaacbb6
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-03 22:53:44 +0200

    Closes [#2] Update DB with new Table FileItems

---
+++ DAL/DAL.FileSharing/_Migrations/20241003203009_CreateFileItemEntity.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.FileSharing._Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateFileItemEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "FileItems",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalHistoryTableName", "FileItemsAudit")
+                        .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalHistoryTableName", "FileItemsAudit")
+                        .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GetDate()")
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalHistoryTableName", "FileItemsAudit")
+                        .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom"),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalHistoryTableName", "FileItemsAudit")
+                        .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom"),
+                    ParentId = table.Column<int>(type: "int", nullable: true)
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalHistoryTableName", "FileItemsAudit")
+                        .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom"),
+                    IsFolder = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalHistoryTableName", "FileItemsAudit")
+                        .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom"),
+                    Display = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true, computedColumnSql: "[Name]", stored: true)
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalHistoryTableName", "FileItemsAudit")
+                        .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom"),
+                    ValidFrom = table.Column<DateTime>(type: "datetime2", nullable: false)
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalHistoryTableName", "FileItemsAudit")
+                        .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom"),
+                    ValidTo = table.Column<DateTime>(type: "datetime2", nullable: false)
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalHistoryTableName", "FileItemsAudit")
+                        .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom"),
+                    TimeStamp = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: false)
+                        .Annotation("SqlServer:IsTemporal", true)
+                        .Annotation("SqlServer:TemporalHistoryTableName", "FileItemsAudit")
+                        .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                        .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                        .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FileItems_Parents",
+                        column: x => x.ParentId,
+                        principalSchema: "dbo",
+                        principalTable: "FileItems",
+                        principalColumn: "Id");
+                })
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "FileItemsAudit")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileItems_ParentId",
+                schema: "dbo",
+                table: "FileItems",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_Path",
+                schema: "dbo",
+                table: "FileItems",
+                columns: new[] { "Name", "ParentId" },
+                unique: true,
+                filter: "[ParentId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileItems",
+                schema: "dbo")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "FileItemsAudit")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom");
+        }
+    }
+}
---
+++ DB/99 - Updates/0.00.02/CREATE TABLE - FileItem.sql
@@ -0,0 +1,83 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241001201945_Initial'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20241001201945_Initial', N'8.0.8');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241003203009_CreateFileItemEntity'
+)
+BEGIN
+    CREATE TABLE [dbo].[FileItems] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(200) NOT NULL,
+        [CreatedAt] datetime2 NOT NULL DEFAULT (GetDate()),
+        [IsDeleted] bit NOT NULL,
+        [ParentId] int NULL,
+        [IsFolder] bit NOT NULL DEFAULT CAST(0 AS bit),
+        [Display] AS [Name] PERSISTED,
+        [ValidFrom] datetime2 GENERATED ALWAYS AS ROW START HIDDEN NOT NULL,
+        [ValidTo] datetime2 GENERATED ALWAYS AS ROW END HIDDEN NOT NULL,
+        [TimeStamp] rowversion NOT NULL,
+        CONSTRAINT [PK_FileItems] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_FileItems_Parents] FOREIGN KEY ([ParentId]) REFERENCES [dbo].[FileItems] ([Id]),
+        PERIOD FOR SYSTEM_TIME([ValidFrom], [ValidTo])
+    ) WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = [dbo].[FileItemsAudit]));
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241003203009_CreateFileItemEntity'
+)
+BEGIN
+    CREATE INDEX [IX_FileItems_ParentId] ON [dbo].[FileItems] ([ParentId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241003203009_CreateFileItemEntity'
+)
+BEGIN
+    EXEC(N'CREATE UNIQUE INDEX [IX_Items_Path] ON [dbo].[FileItems] ([Name], [ParentId]) WHERE [ParentId] IS NOT NULL');
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241003203009_CreateFileItemEntity'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20241003203009_CreateFileItemEntity', N'8.0.8');
+END;
+GO
+
+COMMIT;
+GO
+
