35e83f97a882ddc5fb4881c81cb34c6b6e071605
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-03 22:52:09 +0200

    #2 add FileItem Entity to ApplicationDBContext incl. entity event handlers

--- DAL/DAL.FileSharing/EFStructures/ApplicationDBContext.cs
+++ DAL/DAL.FileSharing/EFStructures/ApplicationDBContext.cs
@@ -1,30 +1,114 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿namespace DAL.FileSharing.EFStructures;
 
-namespace DAL.FileSharing.EFStructures;
 public partial class ApplicationDBContext : DbContext
 {
     #region ctors
     public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
         : base(options)
     {
+        SavingChanges += (sender, args) =>
+        {
+            string cs = ((ApplicationDBContext)sender).Database!.GetConnectionString();
+            Console.WriteLine($"Saving changes for {cs}");
+        };
+        SavedChanges += (sender, args) =>
+        {
+            string cs = ((ApplicationDBContext)sender).Database!.GetConnectionString();
+            Console.WriteLine($"Saved {args!.EntitiesSavedCount} changes for {cs}");
+        };
+        SaveChangesFailed += (sender, args) =>
+        {
+            Console.WriteLine($"An exception occurred! {args.Exception.Message} entities");
+        };
+        ChangeTracker.Tracked += ChangeTracker_Tracked;
+        ChangeTracker.StateChanged += ChangeTracker_StateChanged;
     }
+    #endregion
+
+    #region fields
+    public virtual DbSet<FileItem> FileItems { get; set; }
     #endregion
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        new FileItemConfiguration().Configure(modelBuilder.Entity<FileItem>());
 
         OnModelCreatingPartial(modelBuilder);
     }
-
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    #region mappedDbFunctions
+    // TODO
+    #endregion
+
+    #region eventhandlers
+    private void ChangeTracker_Tracked(object sender, EntityTrackedEventArgs e)
+    {
+        var source = (e.FromQuery) ? "Database" : "Code";
+        if (e.Entry.Entity is FileItem f)
+        {
+            Console.WriteLine($"FileItem entry {f.Name} was added from {source}");
+        }
+    }
+
+    private void ChangeTracker_StateChanged(object sender, EntityStateChangedEventArgs e)
+    {
+        if (e.Entry.Entity is not FileItem f)
+        {
+            return;
+        }
+        var action = string.Empty;
+        Console.WriteLine($"FileItem {f.Name} was {e.OldState} before the state changed to {e.NewState}");
+        switch (e.NewState)
+        {
+            case EntityState.Unchanged:
+                action = e.OldState switch
+                {
+                    EntityState.Added => "Added",
+                    EntityState.Modified => "Edited",
+                    _ => action
+                };
+                Console.WriteLine($"The object was {action}");
+                break;
+        }
+    }
+    #endregion
 
     #region overrides
     protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
     {
         configurationBuilder.Properties<string>().HaveMaxLength(200);
         configurationBuilder.IgnoreAny<INonPersisted>();
+    }
+
+    public override int SaveChanges()
+    {
+        try
+        {
+            return base.SaveChanges();
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            //A concurrency error occurred
+            //Should log and handle intelligently
+            throw new CustomConcurrencyException("A concurrency error happened.", ex);
+        }
+        catch (RetryLimitExceededException ex)
+        {
+            //DbResiliency retry limit exceeded
+            //Should log and handle intelligently
+            throw new CustomRetryLimitExceededException("There is a problem with SQL Server.", ex);
+        }
+        catch (DbUpdateException ex)
+        {
+            //Should log and handle intelligently
+            throw new CustomDbUpdateException("An error occurred updating the database", ex);
+        }
+        catch (Exception ex)
+        {
+            //Should log and handle intelligently
+            throw new CustomException("An error occurred updating the database", ex);
+        }
     }
     #endregion
 }
