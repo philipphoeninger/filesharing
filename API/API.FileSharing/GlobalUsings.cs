de6c84669cf223c673a49eefd79f66a4a1ebd47b
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-05 23:05:58 +0200

    [#14] init GlobalUsings.cs

---
+++ API/API.FileSharing/GlobalUsings.cs
@@ -0,0 +1,34 @@
+﻿global using API.FileSharing.Controllers.Base;
+//global using API.FileSharing.Filters;
+//global using API.FileSharing.Security;
+
+global using DAL.FileSharing.EFStructures;
+global using DAL.FileSharing.Initialization;
+global using DAL.FileSharing.Exceptions;
+global using DAL.FileSharing.Repos;
+global using DAL.FileSharing.Repos.Base;
+global using DAL.FileSharing.Repos.Interfaces;
+
+global using MODELS.FileSharing.Entities;
+global using MODELS.FileSharing.Entities.Base;
+
+global using SERVICES.FileSharing.Utilities;
+
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.ApiExplorer;
+global using Microsoft.AspNetCore.Mvc.Filters;
+global using Microsoft.Extensions.Options;
+global using Microsoft.OpenApi.Any;
+global using Microsoft.OpenApi.Models;
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc.Authorization;
+
+global using System.Reflection;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using System.Net.Http.Headers;
+global using System.Security.Claims;
+global using System.Text;
+global using System.Text.Encodings.Web;
