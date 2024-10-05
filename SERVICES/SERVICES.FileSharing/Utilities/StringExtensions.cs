362d480dfb77b987fd84449127f5426b274fb814
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-05 22:12:23 +0200

    [closes #13] add string extension methods to remove suffixes

---
+++ SERVICES/SERVICES.FileSharing/Utilities/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace SERVICES.FileSharing.Utilities;
+
+public static class StringExtensions
+{
+    public static string RemoveControllerSuffix(this string original)
+       => original.Replace("Controller", "", StringComparison.OrdinalIgnoreCase);
+    public static string RemoveAsyncSuffix(this string original)
+        => original.Replace("Async", "", StringComparison.OrdinalIgnoreCase);
+    public static string RemovePageModelSuffix(this string original)
+        => original.Replace("PageModel", "", StringComparison.OrdinalIgnoreCase);
+}
