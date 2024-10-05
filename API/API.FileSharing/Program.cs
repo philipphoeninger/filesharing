98507d1accdd3a92fbdd0dbad4b9ee95012fc40e
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-05 22:59:30 +0200

    update API Program.cs with SQL Server Connection

--- API/API.FileSharing/Program.cs
+++ API/API.FileSharing/Program.cs
@@ -1,17 +1,34 @@
+[assembly: ApiController]
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// add SQL Server Connection
+var connectionString = builder.Configuration.GetConnectionString("FileSharing");
+builder.Services.AddSqlServer<ApplicationDBContext>(connectionString, options =>
+{
+    options.EnableRetryOnFailure().CommandTimeout(60);
+});
+
+builder.Services.AddRepositories();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Initialize the database
+    if (app.Configuration.GetValue<bool>("RebuildDatabase"))
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDBContext>();
+    }
     app.UseSwagger();
     app.UseSwaggerUI();
 }
