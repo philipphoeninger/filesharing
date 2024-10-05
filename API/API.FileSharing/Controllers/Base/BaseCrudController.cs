68594448c63882329212c3078128747056051bd5
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-05 22:48:43 +0200

    [#14] init Base- & FileItemsController

---
+++ API/API.FileSharing/Controllers/Base/BaseCrudController.cs
@@ -0,0 +1,66 @@
+﻿namespace API.FileSharing.Controllers.Base;
+
+[ApiController]
+[Authorize]
+[Route("api/[controller]")]
+public abstract class BaseCrudController<TEntity, TController> : ControllerBase
+    where TEntity : BaseEntity, new()
+    where TController : class
+{
+    protected readonly IBaseRepo<TEntity> MainRepo;
+
+    protected BaseCrudController(IBaseRepo<TEntity> repo)
+    {
+        MainRepo = repo;
+    }
+
+    /// <summary>
+    /// Gets a single record
+    /// </summary>
+    /// <param name="id">Primary key of the record</param>
+    /// <returns>Single record</returns>
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [SwaggerResponse(200, "The execution was successful")]
+    [SwaggerResponse(204, "No content")]
+    [SwaggerResponse(400, "The request was invalid")]
+    [SwaggerResponse(401, "Unauthorized access attempted")]
+    [HttpGet("{id}")]
+    public ActionResult<TEntity> GetOne(int id)
+    {
+        var entity = MainRepo.Find(id);
+
+        return entity == null ? NoContent() : Ok(entity);
+    }
+
+    // GET: api/<FileItemsController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // POST api/<FileItemsController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    // PUT api/<FileItemsController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    // DELETE api/<FileItemsController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ API/API.FileSharing/Controllers/FileItemsController.cs
@@ -0,0 +1,8 @@
+﻿namespace API.FileSharing.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FileItemsController : BaseCrudController<FileItem, FileItemsController>
+{
+    public FileItemsController(IFileItemRepo repo) : base(repo) { }
+}
