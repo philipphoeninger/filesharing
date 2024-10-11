716a5e0f89008a4a0d346c1ee45d20f56db868e0
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-11 22:48:15 +0200

    init app routing

--- CLIENT/CLIENT.FileSharing/src/app/app.routes.ts
+++ CLIENT/CLIENT.FileSharing/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MyLinksComponent } from './pages/my-links/my-links.component';
+import { SharedComponent } from './pages/shared-component/shared.component';
+import { UploadsComponent } from './pages/uploads/uploads.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'uploads',
+    component: UploadsComponent,
+  },
+  {
+    path: 'myLinks',
+    component: MyLinksComponent,
+  },
+  {
+    path: 'sharedWithMe',
+    component: SharedComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+  },
+];
--- CLIENT/CLIENT.FileSharing/src/app/navigation/navbar/navbar.component.ts
+++ CLIENT/CLIENT.FileSharing/src/app/navigation/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,7 +8,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route?: string;
+};
 
 @Component({
   selector: 'fs-navbar',
@@ -26,10 +32,37 @@ import { RouterOutlet } from '@angular/router';
     MatListModule,
     MatIconModule,
     RouterOutlet,
+    RouterModule,
   ],
   standalone: true,
 })
 export class NavbarComponent implements OnInit {
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'home',
+      label: 'Home',
+      route: 'home',
+    },
+    {
+      icon: 'cloud_upload',
+      label: 'Uploads',
+      route: 'uploads',
+    },
+    {
+      icon: 'link',
+      label: 'My Links',
+      route: 'myLinks',
+    },
+    {
+      icon: 'share',
+      label: 'Shared with me',
+      route: 'sharedWithMe',
+    },
+    {
+      icon: 'logout',
+      label: 'Logout',
+    },
+  ]);
   constructor() {}
 
   ngOnInit() {}
