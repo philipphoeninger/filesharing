5b13d96debfe5216dc4515bff2151b99ba8b15eb
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-12 22:20:21 +0200

    [#24] create initial upload page

--- CLIENT/CLIENT.FileSharing/src/app/pages/uploads/uploads.component.ts
+++ CLIENT/CLIENT.FileSharing/src/app/pages/uploads/uploads.component.ts
@@ -1,44 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { DatePipe } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
-
-export interface Section {
-  name: string;
-  updated: Date;
-}
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import { MatTableModule, MatTable } from '@angular/material/table';
+import { FileItem } from '../../file-items/shared/file-item.model';
+import { FileItemsDataSource } from '../../file-items/shared/mocks/fileItemDataSource';
 
 @Component({
   selector: 'app-uploads',
   standalone: true,
-  imports: [MatListModule, MatIconModule, MatDividerModule, DatePipe],
+  imports: [
+    MatListModule,
+    MatIconModule,
+    MatDividerModule,
+    DatePipe,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+  ],
   templateUrl: './uploads.component.html',
   styleUrl: './uploads.component.scss',
 })
 export class UploadsComponent {
-  folders: Section[] = [
-    {
-      name: 'Photos',
-      updated: new Date('1/1/16'),
-    },
-    {
-      name: 'Recipes',
-      updated: new Date('1/17/16'),
-    },
-    {
-      name: 'Work',
-      updated: new Date('1/28/16'),
-    },
-  ];
-  notes: Section[] = [
-    {
-      name: 'Vacation Itinerary',
-      updated: new Date('2/20/16'),
-    },
-    {
-      name: 'Kitchen Remodel',
-      updated: new Date('1/18/16'),
-    },
-  ];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<FileItem>;
+  dataSource = new FileItemsDataSource();
+  displayedColumns = ['name', 'fileSize', 'createdAt'];
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
 }
