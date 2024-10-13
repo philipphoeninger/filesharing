3f35dc8cfd11be1b143412fe0beb2f3c25ea1134
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-13 02:11:22 +0200

    [#18] create file type groups mock data (client)

---
+++ CLIENT/CLIENT.FileSharing/src/app/file-items/shared/mocks/fileTypeGroups.mock.data.ts
@@ -0,0 +1,23 @@
+import { IFileTypeGroup } from '../file-type-group.interface';
+import { EnFileType } from '../file-type.enum';
+
+export let MOCK_FILE_TYPE_GROUPS: IFileTypeGroup[] = [
+  {
+    name: 'General',
+    fileTypes: [
+      { value: EnFileType.Document, label: 'Document' },
+      { value: EnFileType.Folder, label: 'Folder' },
+      { value: EnFileType.Image, label: 'Images & Pictures' },
+      { value: EnFileType.Video, label: 'Videos' },
+      { value: EnFileType.Other, label: 'Other' },
+    ],
+  },
+  {
+    name: 'Specific',
+    fileTypes: [
+      { value: EnFileType.Table, label: 'Table' },
+      { value: EnFileType.Folder, label: 'Form' },
+      { value: EnFileType.Zip, label: 'Zip' },
+    ],
+  },
+];
