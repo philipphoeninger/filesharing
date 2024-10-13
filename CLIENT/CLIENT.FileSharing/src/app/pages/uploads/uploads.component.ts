acce3784dd10d7bfc068c17cfe3ffba4dad1bb6c
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-13 02:39:34 +0200

    [#24] add actions column to uploads table

--- CLIENT/CLIENT.FileSharing/src/app/pages/uploads/uploads.component.ts
+++ CLIENT/CLIENT.FileSharing/src/app/pages/uploads/uploads.component.ts
@@ -44,7 +44,7 @@ export class UploadsComponent {
   @ViewChild(MatTable) table!: MatTable<FileItem>;
   @ViewChild(MatMenuTrigger) contextMenu!: MatMenuTrigger;
   dataSource = new FileItemsDataSource();
-  displayedColumns = ['name', 'fileSize', 'createdAt'];
+  displayedColumns = ['name', 'fileSize', 'createdAt', 'actions'];
 
   fileTypeControl = new FormControl('');
   fileTypeGroups = MOCK_FILE_TYPE_GROUPS;
