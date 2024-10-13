d19188c752ef630ccf1839dd858c477b4d9472eb
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-13 02:17:52 +0200

    [#18] create time span mock data (client)

---
+++ CLIENT/CLIENT.FileSharing/src/app/file-items/shared/mocks/timeSpans.mock.data.ts
@@ -0,0 +1,11 @@
+import { EnTimeSpan } from '../time-span.enum';
+import { ITimeSpan } from '../time-span.interface';
+
+export let MOCK_TIME_SPANS: ITimeSpan[] = [
+  { value: EnTimeSpan.Today, label: 'Today' },
+  { value: EnTimeSpan.Yesterday, label: 'Yesterday' },
+  { value: EnTimeSpan.Last7Days, label: 'Last 7 days' },
+  { value: EnTimeSpan.Last30Days, label: 'Last 30 days' },
+  { value: EnTimeSpan.ThisYear, label: 'This year' },
+  { value: EnTimeSpan.LastYear, label: 'Last year' },
+];
