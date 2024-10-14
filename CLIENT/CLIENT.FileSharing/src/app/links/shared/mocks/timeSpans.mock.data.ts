a5af70da8e389e43bf23dc96ef1f696b4fff2b1e
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-14 23:40:07 +0200

    [#24] create time spans mock data

---
+++ CLIENT/CLIENT.FileSharing/src/app/links/shared/mocks/timeSpans.mock.data.ts
@@ -0,0 +1,9 @@
+import { ITimeSpan } from '../../../file-items/shared/time-span.interface';
+import { EnTimeSpanFuture } from '../time-span.enum';
+
+export let MOCK_TIME_SPANS_FUTURE: ITimeSpan[] = [
+  { value: EnTimeSpanFuture.Today, label: 'Today' },
+  { value: EnTimeSpanFuture.Tomorrow, label: 'Tomorrow' },
+  { value: EnTimeSpanFuture.Next7Days, label: 'Next 7 days' },
+  { value: EnTimeSpanFuture.Next30Days, label: 'Next 30 days' },
+];
