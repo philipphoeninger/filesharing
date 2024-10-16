ddc84840904e98415def86d66ceda6996edb09c3
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-16 21:36:13 +0200

    [#30] setup FileItemsApiService

--- CLIENT/CLIENT.FileSharing/src/app/file-items/shared/fileItems-api.service.ts
+++ CLIENT/CLIENT.FileSharing/src/app/file-items/shared/fileItems-api.service.ts
@@ -1,6 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { httpAppConfig } from '../../app.config';
+import { FileItem } from './file-item.model';
+import { Observable, of as observableOf } from 'rxjs';
+import { MOCK_FILE_ITEMS } from './mocks/fileItems.mock.data';
 
 @Injectable({ providedIn: 'root' })
 export class FileItemsApiService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getFileItems(): Observable<FileItem[]> {
+    // return this.http.get<FileItem[]>(`${httpAppConfig.apiEndpoint}/file-items`);
+    return observableOf(MOCK_FILE_ITEMS);
+  }
+
+  getFileItem(id: number): Observable<FileItem | null> {
+    // return this.http.get<FileItem>(`${httpAppConfig.apiEndpoint}/file-items/${id}`);
+    return observableOf(
+      MOCK_FILE_ITEMS.find((fileItem) => fileItem.id === id) || null,
+    );
+  }
 }
