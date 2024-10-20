d19dcbe9489a07a304ac046ee13f707050e2004f
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-20 17:50:46 +0200

    create App config extension methods

---
+++ API/API.FileSharing/Extensions/AppConfigExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace API.FileSharing.Extensions;
+
+public static class AppConfigExtensions
+{
+    public static WebApplication ConfigureCORS(this WebApplication app, IConfiguration config)
+    {
+        app.UseCors();
+        return app;
+    }
+
+    public static IServiceCollection AddAppConfig(this IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<JWTSettings>(config.GetSection("JWTSettings"));
+        return services;
+    }
+}
