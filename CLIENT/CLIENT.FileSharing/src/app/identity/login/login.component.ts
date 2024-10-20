730a177b952f2e9a89f954a11a3578ace8b8a783
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-21 00:29:37 +0200

    [#6] create first login page design

--- CLIENT/CLIENT.FileSharing/src/app/identity/login/login.component.ts
+++ CLIENT/CLIENT.FileSharing/src/app/identity/login/login.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
-  selector: 'app-login',
+  selector: 'fs-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrl: './login.component.scss',
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+  ],
 })
 export class LoginComponent implements OnInit {
+  public loginErrorMessage?: string;
+  public showLanguageSelection = true;
 
-  constructor() { }
+  public languages = [
+    { value: 'en', label: 'English' },
+    { value: 'de', label: 'Deutsch' },
+    { value: 'fr', label: 'Français' },
+  ];
+  public selectedLanguage = 'en';
 
-  ngOnInit() {
+  protected username?: string;
+  protected password?: string;
+  public keepSignedIn: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSubmit(form: NgForm, event: Event) {
+    event.preventDefault();
+    console.log('Form submitted', form.value);
   }
-
 }
