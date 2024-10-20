8711decf314dd170d8190929e9273d0d5c5666f9
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-20 18:10:51 +0200

    relocate Identity config to extension methods & add JWT Authentication configuration

---
+++ API/API.FileSharing/Extensions/IdentityExtensions.cs
@@ -0,0 +1,56 @@
+﻿namespace API.FileSharing.Extensions;
+
+public static class IdentityExtensions
+{
+    public static IServiceCollection AddIdentityHandlersAndStores(this IServiceCollection services)
+    {
+        services.AddIdentityCore<User>()
+                .AddEntityFrameworkStores<ApplicationDBContext>()
+                .AddApiEndpoints();
+        return services;
+    }
+
+    public static IServiceCollection ConfigureIdentityOptions(this IServiceCollection services)
+    {
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+        });
+        return services;
+    }
+
+    // Auth = Authentication + Authorization
+    public static IServiceCollection AddIdentityAuth(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme =
+            options.DefaultChallengeScheme =
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }) // JwtBearerDefaults.AuthenticationScheme
+        .AddJwtBearer(options =>
+        {
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = config["JWTSettings:Issuer"],
+                ValidAudience = config["JWTSettings:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWTSettings:Key"]!)),
+                ClockSkew = TimeSpan.FromMinutes(2)
+            };
+        });
+        services.AddAuthorization();
+        return services;
+    }
+
+    public static WebApplication AddIdentityAuthMiddlewares(this WebApplication app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+        return app;
+    }
+}
