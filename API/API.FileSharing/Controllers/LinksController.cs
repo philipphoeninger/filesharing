132565775c73882e4941a353710ae222c2b670c9
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-20 17:44:54 +0200

    [#28] init LinksController

---
+++ API/API.FileSharing/Controllers/LinksController.cs
@@ -0,0 +1,12 @@
+﻿namespace API.FileSharing.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LinksController : BaseCrudController<Link, LinksController>
+{
+    public LinksController(IAppLogging<LinksController> logger, ILinkRepo repo) : base(logger, repo)
+    {
+    }
+
+    // TODO: put more individual Requests here
+}
