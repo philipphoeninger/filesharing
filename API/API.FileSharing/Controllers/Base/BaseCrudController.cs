9f8e0adddf9bb203adede0e63ec6c82bb8cb7392
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-20 17:46:54 +0200

    update BaseCrudController to support API versioning & add CRUD methods

--- API/API.FileSharing/Controllers/Base/BaseCrudController.cs
+++ API/API.FileSharing/Controllers/Base/BaseCrudController.cs
@@ -1,8 +1,9 @@
-namespace API.FileSharing.Controllers.Base;
+﻿namespace API.FileSharing.Controllers.Base;
 
 [ApiController]
 [Authorize]
 [Route("api/[controller]")]
+[Route("api/v{version:apiVersion}/[controller]")]
 public abstract class BaseCrudController<TEntity, TController> : ControllerBase
     where TEntity : BaseEntity, new()
     where TController : class
@@ -21,6 +22,8 @@ public abstract class BaseCrudController<TEntity, TController> : ControllerBase
     /// </summary>
     /// <param name="id">Primary key of the record</param>
     /// <returns>Single record</returns>
+    [HttpGet("{id}")]
+    [ApiVersion("0.1")]
     [Produces("application/json")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
@@ -30,7 +33,6 @@ public abstract class BaseCrudController<TEntity, TController> : ControllerBase
     [SwaggerResponse(204, "No content")]
     [SwaggerResponse(400, "The request was invalid")]
     [SwaggerResponse(401, "Unauthorized access attempted")]
-    [HttpGet("{id}")]
     public ActionResult<TEntity> GetOne(int id)
     {
         var entity = MainRepo.Find(id);
@@ -38,31 +40,133 @@ public abstract class BaseCrudController<TEntity, TController> : ControllerBase
         return entity == null ? NoContent() : Ok(entity);
     }
 
-    // GET: api/<FileItemsController>
+    /// <summary>
+    /// Gets all records
+    /// </summary>
+    /// <returns>All records</returns>
     [HttpGet]
-    public IEnumerable<string> Get()
+    [ApiVersion("0.1")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [SwaggerResponse(200, "The execution was successful")]
+    [SwaggerResponse(400, "The request was invalid")]
+    [SwaggerResponse(401, "Unauthorized access attempted")]
+    public ActionResult<IEnumerable<string>> GetAll()
     {
-        return new string[] { "value1", "value2" };
+        return Ok(MainRepo.GetAllIgnoreQueryFilters());
     }
 
-    // POST api/<FileItemsController>
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-        throw new NotImplementedException();
-    }
-
-    // PUT api/<FileItemsController>/5
+    /// <summary>
+    /// Updates a single record
+    /// </summary>
+    /// <remarks>
+    /// Sample body:
+    /// <pre>
+    /// {
+    ///     ...
+    /// }
+    /// </pre>
+    /// </remarks>
+    /// <param name="id">Primary key of the record to update</param>
+    /// <param name="entity">Entity to update</param>
+    /// <returns>Single record</returns>
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
+    [ApiVersion("0.1")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [SwaggerResponse(200, "The execution was successful")]
+    [SwaggerResponse(400, "The request was invalid")]
+    [SwaggerResponse(401, "Unauthorized access attempted")]
+    public IActionResult UpdateOne(int id, TEntity entity)
     {
-        throw new NotImplementedException();
+        if (id != entity.Id) return BadRequest();
+        if (!ModelState.IsValid) return ValidationProblem(ModelState);
+        try
+        {
+            MainRepo.Update(entity);
+        }
+        catch (CustomException ex)
+        {
+            // TODO: handle more gracefully
+            return BadRequest(ex);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex);
+        }
+        return Ok(entity);
     }
 
-    // DELETE api/<FileItemsController>/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
+    /// <summary>
+    /// Adds a single record
+    /// </summary>
+    /// <remarks>
+    /// Sample body:
+    /// <pre>
+    /// {
+    ///     ...
+    /// }
+    /// </pre>
+    /// </remarks>
+    /// <param name="entity">Record to add</param>
+    /// <returns>Added record</returns>
+    [HttpPost]
+    [ApiVersion("0.1")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [SwaggerResponse(201, "The execution was successful")]
+    [SwaggerResponse(400, "The request was invalid")]
+    [SwaggerResponse(401, "Unauthorized access attempted")]
+    public ActionResult<TEntity> AddOne(TEntity entity)
     {
-        throw new NotImplementedException();
+        if (!ModelState.IsValid) return ValidationProblem(ModelState);
+        try
+        {
+            MainRepo.Add(entity);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex);
+        }
+        return CreatedAtAction(nameof(GetOne), new { id = entity.Id }, entity);
+    }
+
+    /// <summary>
+    /// Deletes a single record
+    /// </summary>
+    /// <remarks>
+    /// Sample body:
+    /// </remarks>
+    /// <param name="id"></param>
+    /// <param name="entity"></param>
+    /// <returns></returns>
+    [HttpDelete("{id}")]
+    [ApiVersion("0.1")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [SwaggerResponse(200, "The execution was successful")]
+    [SwaggerResponse(400, "The request was invalid")]
+    [SwaggerResponse(401, "Unauthorized access attempted")]
+    public ActionResult<TEntity> DeleteOne(int id, TEntity entity)
+    {
+        if (id != entity.Id) return BadRequest();
+        try
+        {
+            MainRepo.Delete(entity);
+        }
+        catch (Exception ex)
+        {
+            // TODO: handle more gracefully
+            return new BadRequestObjectResult(ex.GetBaseException()?.Message);
+        }
+        return Ok();
     }
 }
