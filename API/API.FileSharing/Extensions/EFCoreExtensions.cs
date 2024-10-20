3df39d9834e696786a49dfb4cc25c098b66006e0
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-20 17:52:11 +0200

    relocate EF Core configuration to extension methods

---
+++ API/API.FileSharing/Extensions/EFCoreExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace API.FileSharing.Extensions;
+
+public static class EFCoreExtensions
+{
+    public static IServiceCollection AddSqlServerConnection(this IServiceCollection services, IConfiguration config)
+    {
+        string? connectionString = config.GetConnectionString("FileSharing");
+        services.AddSqlServer<ApplicationDBContext>(connectionString, options =>
+        {
+            options.EnableRetryOnFailure().CommandTimeout(60);
+        });
+        return services;
+    }
+}
