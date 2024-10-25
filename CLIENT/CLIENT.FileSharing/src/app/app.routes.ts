61592ee73a99903c2c4b028f5e1a44390f2a88cc
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-26 00:48:46 +0200

    create auth guard & protect routes

--- CLIENT/CLIENT.FileSharing/src/app/app.routes.ts
+++ CLIENT/CLIENT.FileSharing/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { LoginComponent } from './identity/login/login.component';
 import { RegisterComponent } from './identity/register/register.component';
+import { AuthGuard } from './shared/auth.guard';
 
 export const routes: Routes = [
   {
@@ -25,25 +26,31 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'uploads',
     component: UploadsComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'myLinks',
     component: MyLinksComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'sharedWithMe',
     component: SharedComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'profile',
     component: ProfileComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'settings',
     component: SettingsComponent,
+    canActivate: [AuthGuard],
   },
 ];
--- CLIENT/CLIENT.FileSharing/src/app/identity/shared/auth.service.ts
+++ CLIENT/CLIENT.FileSharing/src/app/identity/shared/auth.service.ts
@@ -4,9 +4,10 @@ import { httpAppConfig } from '../../app.config';
 import { Observable } from 'rxjs';
 import { LoginModel } from './login.model';
 import { RegisterModel } from './register.model';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({ providedIn: 'root' })
-export class IdentityApiService {
+export class AuthService {
   constructor(private http: HttpClient) {}
 
   login(command: LoginModel): Observable<any> {
@@ -15,5 +16,12 @@ export class IdentityApiService {
 
   register(command: RegisterModel): Observable<any> {
     return this.http.post<any>(`${httpAppConfig.apiEndpoint}/signUp`, command);
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('fileshare-token');
+    const helper = new JwtHelperService();
+    const isExpired = helper.isTokenExpired(token);
+    return !isExpired;
   }
 }
