1c2cbfb59deb30d998a8975184157b5382858d21
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-28 00:06:30 +0100

    update DAL.GlobalUsings

--- DAL/DAL.FileSharing/GlobalUsings.cs
+++ DAL/DAL.FileSharing/GlobalUsings.cs
@@ -2,11 +2,11 @@
 global using DAL.FileSharing.Exceptions;
 global using DAL.FileSharing.Repos.Base;
 global using DAL.FileSharing.Repos.Interfaces;
+global using DAL.FileSharing.Initialization;
 
 global using MODELS.FileSharing.Entities;
 global using MODELS.FileSharing.Entities.Base;
 global using MODELS.FileSharing.Entities.Configuration;
-
 global using MODELS.FileSharing.ViewModels;
 global using MODELS.FileSharing.ViewModels.Interfaces;
 //global using MODELS.FileSharing.ViewModels.Configuration;
@@ -18,6 +18,8 @@ global using Microsoft.Data.SqlClient;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.ChangeTracking;
 global using Microsoft.EntityFrameworkCore.Design;
+global using Microsoft.EntityFrameworkCore.Diagnostics;
+global using Microsoft.EntityFrameworkCore.Infrastructure;
 global using Microsoft.EntityFrameworkCore.Metadata;
 global using Microsoft.EntityFrameworkCore.Migrations;
 global using Microsoft.EntityFrameworkCore.Query;
