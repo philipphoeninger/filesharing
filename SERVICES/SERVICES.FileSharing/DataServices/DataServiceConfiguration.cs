7209b34ba74fb1995b1ddde0bd69b4820629d801
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-27 23:55:05 +0100

    [#28] add LinkRepo to DataServices ServiceCollection

--- SERVICES/SERVICES.FileSharing/DataServices/DataServiceConfiguration.cs
+++ SERVICES/SERVICES.FileSharing/DataServices/DataServiceConfiguration.cs
@@ -1,10 +1,11 @@
-﻿namespace SERVICES.FileSharing.DataServices;
+namespace SERVICES.FileSharing.DataServices;
 
 public static class DataServiceConfiguration
 {
     public static IServiceCollection AddRepositories(this IServiceCollection services)
     {
         services.AddScoped<IFileItemRepo, FileItemRepo>();
+        services.AddScoped<ILinkRepo, LinkRepo>();
         
         return services;
     }
