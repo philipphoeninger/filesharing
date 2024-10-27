e870269b638590129fa260a14a59ed7550024482
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-27 18:48:47 +0100

    [#30] fix stackoverflow caused by recursive query filter

--- DAL/MODELS.FileSharing/Entities/Configuration/FileItemConfiguration.cs
+++ DAL/MODELS.FileSharing/Entities/Configuration/FileItemConfiguration.cs
@@ -1,4 +1,4 @@
-﻿namespace MODELS.FileSharing.Entities.Configuration;
+namespace MODELS.FileSharing.Entities.Configuration;
 
 public class FileItemConfiguration : IEntityTypeConfiguration<FileItem>
 {
@@ -6,7 +6,6 @@ public class FileItemConfiguration : IEntityTypeConfiguration<FileItem>
     {
         // query filters
         builder.HasQueryFilter(f => !f.IsDeleted);
-        builder.HasQueryFilter(f => !f.ParentNavigation!.IsDeleted);
 
         // properties
         builder.Property(f => f.IsFolder)
