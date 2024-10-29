d351e364c14ce464c2aa975d73113df1b7293f3e
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-29 21:35:05 +0100

    [#24] create file-items response model (BE)

---
+++ API/API.FileSharing/Controllers/Models/GetFileItemResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace API.FileSharing.Controllers.Models;
+
+public class GetFileItemResponse
+{
+    public FileItem FileItem { get; set; }
+
+    public List<PathModel> DirectoryPath { get; set; }
+
+    public GetFileItemResponse() { }
+}
