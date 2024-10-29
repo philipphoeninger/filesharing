a9da16a6145c9074c3ac66acda7a086e4d553149
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-29 22:48:07 +0100

    [#24] add DB make owner-fileItemName-parentId unique migration

---
+++ DAL/DAL.FileSharing/_Migrations/20241029214331_MakeOwnerFileItemNameParentIdUnique.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.FileSharing._Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeOwnerFileItemNameParentIdUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_FileItems_Owner",
+                schema: "dbo",
+                table: "FileItems");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_FileItems_Folder",
+                schema: "dbo",
+                table: "FileItems",
+                columns: new[] { "Owner", "Name", "ParentId" },
+                unique: true,
+                filter: "[ParentId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_User_FileItems_Folder",
+                schema: "dbo",
+                table: "FileItems");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileItems_Owner",
+                schema: "dbo",
+                table: "FileItems",
+                column: "Owner");
+        }
+    }
+}
