ff94487bf29be42dbba620e4f5c26580ee9787da
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-10-29 21:44:00 +0100

    [#24] update file-items controller to support Get including directory path

--- API/API.FileSharing/Controllers/FileItemsController.cs
+++ API/API.FileSharing/Controllers/FileItemsController.cs
@@ -1,4 +1,4 @@
-﻿namespace API.FileSharing.Controllers;
+namespace API.FileSharing.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
@@ -6,6 +6,57 @@ public class FileItemsController : BaseCrudController<FileItem, FileItemsControl
 {
     public FileItemsController(IAppLogging<FileItemsController> logger, IFileItemRepo repo) : base(logger, repo)
     {
+    }
+
+    /// <summary>
+    /// Gets a single record & it's corresponding path
+    /// </summary>
+    /// <param name="id">Primary key of the record</param>
+    /// <returns>Single record</returns>
+    [HttpGet("withPath/{id}")]
+    [ApiVersion("0.1")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [SwaggerResponse(200, "The execution was successful")]
+    [SwaggerResponse(204, "No content")]
+    [SwaggerResponse(400, "The request was invalid")]
+    [SwaggerResponse(401, "Unauthorized access attempted")]
+    public ActionResult<GetFileItemResponse> GetOneWithPath(int id)
+    {
+        var entity = MainRepo.Find(id);
+        if (entity == null) return NoContent();
+
+        List<PathModel> path = [];
+        path.Add(new PathModel()
+        {
+            FileItemId = entity.Id,
+            PathName = entity.Name,
+            SortId = 0
+        });
+        var parent = entity.ParentNavigation;
+        int sortId = 1;
+        while (parent != null)
+        {
+            PathModel parentPath = new PathModel()
+            {
+                FileItemId = parent.Id,
+                PathName = parent.Name,
+                SortId = sortId
+            };
+            path.Add(parentPath);
+            sortId += 1;
+            parent = parent.ParentNavigation;
+        }
+        GetFileItemResponse response = new GetFileItemResponse()
+        {
+            FileItem = entity,
+            DirectoryPath = path
+        };
+
+        return Ok(response);
     }
 
     // TODO: put more individual Requests here
